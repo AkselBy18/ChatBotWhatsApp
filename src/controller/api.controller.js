adee18729ac1856da2cf2f38b02ccaadc7e4fc98
AkselBy18 <akselherrera18@gmail.com>
2024-06-10 14:42:23 -0700

    Server conf

--- src/controller/api.controller.js
+++ src/controller/api.controller.js
@@ -1,5 +1,17 @@
 const verify = (req, res) => {
-    res.send("Verificado");
+    try {
+        const accessToken = "TOKEN_WEB_FOR_META";
+        const token = req.query["hub.verify_token"];
+        const challenge = req.query["hub.challenge"];
+
+        if(challenge && token && (accessToken == token)) {
+            res.send(challenge);
+        } else {
+            res.status(400).send({error: 'can not get token or challenge'});
+        }
+    } catch (error) {
+        res.status(400).send(error);
+    }
 }
 
 const recive = (req, res) => {
