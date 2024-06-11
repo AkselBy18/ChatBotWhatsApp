1b09ef7a8d3505c52a5667b71e0c17029011bda9
AkselBy18 <akselherrera18@gmail.com>
2024-06-11 08:57:21 -0700

    Conexión con Meta

--- src/controller/api.controller.js
+++ src/controller/api.controller.js
@@ -9,13 +9,24 @@ const verify = (req, res) => {
         } else {
             res.status(400).send({error: 'can not get token or challenge'});
         }
+        console.log(req);
     } catch (error) {
         res.status(400).send(error);
     }
 }
 
 const recive = (req, res) => {
-    res.send("Recivido");
+    try {
+        const entry = (req.body["entry"])[0];
+        const change = (entry.changes[0]);
+        console.log("---------------------- SHOW ENTRY ----------------------");
+        console.log(entry);
+        console.log(JSON.stringify(change));
+        res.send("EVENT_RECEIVED");
+    } catch (error) {
+        console.log("Error", error);
+        res.send("EVENT_RECEIVED");
+    }
 }
 
 module.exports = {
